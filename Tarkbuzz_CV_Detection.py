e97ffcb025ee421b44ff5e143b873da9df092945
BaxDoggie <joeybrowerphoto@gmail.com>
2026-09-16 17:46:18 -0400

    Add detect_head_damage function for assessing damage levels based on color detection

--- Tarkbuzz_CV_Detection.py
+++ Tarkbuzz_CV_Detection.py
@@ -27,7 +27,37 @@ def damage_colour(frame, lower_hsv, upper_hsv):
 
     return cv2.countNonZero(mask) / total
 
+def detect_head_damage(frame):
+    if frame is None or frame.size == 0:
+        return 0.0
+
+    if frame.shape[-1] == 4:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+
+    green = damage_colour(frame, np.array([35, 50, 50]), np.array([90, 255, 255]))
+    yellow = damage_colour(frame, np.array([15, 100, 100]), np.array([30, 255, 255]))
+    orange = damage_colour(frame, np.array([5, 80, 80]), np.array([20, 255, 255]))
+    red1 = damage_colour(frame, np.array([0, 80, 80]), np.array([10, 255, 255]))
+    red2 = damage_colour(frame, np.array([170, 80, 80]), np.array([180, 255, 255]))
+    red = max(red1, red2)
+
+    if red > 0.08:
+        return 1.0
+    if orange > 0.08:
+        return 0.75
+    if yellow > 0.08:
+        return 0.5
+    if green > 0.05:
+        return 0.0
+
+    return 0.0
 
 
-    
-  
+
+
+
+
+
+
+
+
